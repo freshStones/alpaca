9917ceb3efbf78bdb62d9d5ce4dfb35ef70b5885
zghember <zghpku@gmail.com>
2013-11-03 15:49:03 +0800

    2013-11-03 mor

--- qunaertest.py
+++ qunaertest.py
@@ -4,11 +4,20 @@ import math
 reload(sys)
 sys.setdefaultencoding('utf-8')
 from selenium import webdriver
-for i in range(0,50):
-	driver = webdriver.Firefox()
-	driver.get('http://flight.qunar.com/site/oneway_list.htm?searchDepartureAirport=%E5%8C%97%E4%BA%AC&searchArrivalAirport=%E4%B8%8A%E6%B5%B7&searchDepartureTime=2013-10-29&searchArrivalTime=2013-10-29&nextNDays=0&sd_idx=0&SearchLocation=sevenday_search');
-	time.sleep(30)
-	html = driver.page_source
-	data = driver.find_element_by_id('hdivResultPanel').text
-	driver.quit()
-print data
+driver = webdriver.Firefox()
+driver.get('http://flight.qunar.com/site/oneway_list.htm?searchDepartureAirport=%E5%8C%97%E4%BA%AC&searchArrivalAirport=%E4%B8%8A%E6%B5%B7&searchDepartureTime=2013-10-29&searchArrivalTime=2013-10-29&nextNDays=0&sd_idx=0&SearchLocation=sevenday_search');
+html = driver.page_source
+time.sleep(10)
+a = 1
+while  a == 1:
+	try:
+		time.sleep(10)
+		data = driver.find_element_by_id('hdivResultPanel').text
+		print data
+		nextp = driver.find_element_by_id('nextXI3')
+		nextp.click()
+	except Exception as e:
+		a = 0
+driver.quit()
+
+
