d1b9e85c28a7db601a896e72f1604df94a460855
wanderingJoker <pkucs.lsl@gmail.com>
2013-11-03 16:10:56 +0800

    generate datagrab.txt

--- qunaertest.py
+++ qunaertest.py
@@ -8,16 +8,23 @@ driver = webdriver.Firefox()
 driver.get('http://flight.qunar.com/site/oneway_list.htm?searchDepartureAirport=%E5%8C%97%E4%BA%AC&searchArrivalAirport=%E4%B8%8A%E6%B5%B7&searchDepartureTime=2013-10-29&searchArrivalTime=2013-10-29&nextNDays=0&sd_idx=0&SearchLocation=sevenday_search');
 html = driver.page_source
 time.sleep(10)
+
+file_object = open("datagrab.txt",'w')
+
 a = 1
 while  a == 1:
 	try:
 		time.sleep(10)
 		data = driver.find_element_by_id('hdivResultPanel').text
-		print data
+#		print data
+		file_object.write(data)
+
 		nextp = driver.find_element_by_id('nextXI3')
 		nextp.click()
 	except Exception as e:
 		a = 0
+		
+file_object.close()
 driver.quit()
 
 
