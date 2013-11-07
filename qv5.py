76ce72525f64230024346dfd47532d4d402fc95f
zghember <zghpku@gmail.com>
2013-11-07 23:30:59 +0800

    great version

--- qv5.py
+++ qv5.py
@@ -13,10 +13,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
-def dbinsert(conn,cur,flightNo,isTransferred,flightSequence,airplaneType,flightDate,departureTime,arrivalTime,departureAirport,arrivalAirport,lowestPrice,quatationSource):	
+def dbinsert(conn,cur,flightNo,isTransferred,flightSequence,airplaneType,flightDate,departureTime,arrivalTime,departureAirport,arrivalAirport,lowestPrice,quotationSource):	
 	today = datetime.datetime.now()
-	query ="insert into onlinePrice(flightNo,isTransferred,flightSequence,airplaneType,flightDate,departureTime,arrivalTime,departureAirport,arrivalAirport,lowestPrice,quatationSource,accquiredTime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,from_unixtime(%s))"
-	value = (flightNo,isTransferred,flightSequance,airplaneType,flightDate,departureTime,arrivalTime,departureAirport,arrivalAirport,lowestPrice,quatationSource,time.mktime(today.timetuple())
+	query ="insert into FlightPriceQuotation(flightNo,isTransferred,flightSequence,airplaneType,flightDate,departureTime,arrivalTime,departureAirport,arrivalAirport,lowestPrice,quotationSource,accquiredTime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,from_unixtime(%s))"
+	value = (flightNo,isTransferred,flightSequence,airplaneType,flightDate,departureTime,arrivalTime,departureAirport,arrivalAirport,lowestPrice,quotationSource,time.mktime(today.timetuple()))
 	cur.execute(query,value);
 	conn.commit()
 
@@ -48,7 +48,14 @@ def onepage(dep,arv,ti,src):
 
 	driver = webdriver.Firefox()
 	driver.get(url)
-	webdriver.support.wait.WebDriverWait(driver,40).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'dec'),'搜索结束'))
+	try:
+		webdriver.support.wait.WebDriverWait(driver,20).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'msg2'),'搜索结束'))
+		driver.quit()
+		return
+	except Exception as e:
+		print "have flight from "+dep+''+arv
+
+		webdriver.support.wait.WebDriverWait(driver,30).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'dec'),'搜索结束'))
 	script = "var editPrice = function(price, pos, num) { var t = price.split(''); t[t.length - pos] = num; return t.join(''); }; var getPriceById = function(itemId) { var prices = document.getElementById(itemId).getElementsByClassName('prc')[0].getElementsByTagName('b'); var width = 0; var price = 0; for (var i = 0; i < prices.length; ++i) { if (prices[i].style.width) { width = prices[i].style.width; width = width.substring(0, width.indexOf('px')); price = prices[i].innerHTML; } else { var left = prices[i].style.left; left = Math.abs(left.substring(0, left.indexOf('px'))); price = editPrice(price, left / 11, prices[i].innerHTML); } } for (var i = prices.length - 1; i > 0; --i) { prices[i].parentNode.removeChild(prices[i]); } prices[0].innerHTML = price; return price; }; var key = new Array(); var priceArray = new Array(); var d = document.getElementById('hdivResultPanel').children; for (var i = 0; i < d.length; ++i) { key[i] = d[i].id; priceArray[i] = getPriceById(d[i].id); } "
 
 	a = 1
@@ -78,6 +85,7 @@ def onepage(dep,arv,ti,src):
 					dbinsert(conn,cur,fl_name,istrans,'SECOND',fl_model,ti,tm_dep,tm_arv,local_dep,local_arv,prc,src)
 
 				except Exception as e:
+					print e
 					print 'origin';
 #print e
 				if istrans == 0:
@@ -86,6 +94,7 @@ def onepage(dep,arv,ti,src):
 						(fl_name,fl_model,tm_dep,tm_arv,local_dep,local_arv)=calcnode(origin,src)
 						dbinsert(conn,cur,fl_name,istrans,'FIRST',fl_model,ti,tm_dep,tm_arv,local_dep,local_arv,prc,src)
 					except Exception as e:
+						print e
 						print 'trans'	
 
 			nextp = driver.find_element_by_id('nextXI3')
@@ -101,5 +110,8 @@ f = open('city.lst')
 text = f.read();
 line = text.split('\n')
 print line
-return 
 onepage('北京','上海','2013-11-16','qunar.com')
+for i in line:
+	for j in line:
+		if i!=j:
+			onepage(i,j,2013-11-15,'qunar.com')
