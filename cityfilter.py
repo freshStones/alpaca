7a40e441027c99a1fd69c1f8508c3e2e07ae497e
wanderingJoker <pkucs.lsl@gmail.com>
2013-11-10 21:47:15 +0800

    nothing

--- cityfilter.py
+++ cityfilter.py
@@ -44,13 +44,13 @@ def onepage(dep,arv,ti,src):
 	flagex = 0
 	flagin = 0
 	try:
-		webdriver.support.wait.WebDriverWait(driver,20).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'msg2'),'搜索结束'))
+		webdriver.support.wait.WebDriverWait(driver,40).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'msg2'),'搜索结束'))
 		flagex = 1
 		f1.write(dep+' '+arv+'\n')
 	except Exception as e:
 		f2.flush()
 	try:	
-		webdriver.support.wait.WebDriverWait(driver,30).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'dec'),'搜索结束'))
+		webdriver.support.wait.WebDriverWait(driver,50).until(webdriver.support.expected_conditions.text_to_be_present_in_element((By.CLASS_NAME,'dec'),'搜索结束'))
 		flagin = 1
 		f2.write(dep+' '+arv+'\n')
 	except Exception as e:
@@ -58,7 +58,7 @@ def onepage(dep,arv,ti,src):
 	if (flagin == 0 and flagex ==0):
 		f3.write(dep+' '+arv+'\n')
 
-	driver.quit():
+	driver.quit()
 
 f = open('lst/piece2.lst')
 f1 = open('lst/exclude2.lst','w')
@@ -81,10 +81,14 @@ for i in line:
 #	thread.start_new_thread(onepage,(dep,arv,'2013-11-13','qunar.com'))
 	t = threading.Thread(target=onepage,args=(dep,arv,'2013-11-13','qunar.com'))
 	t.start()
+	f1.flush()
+	f2.flush()
+	f3.flush()
 	print 'new'
 	time.sleep(1)
-	while (len(threading.enumerate())>5):
+	while (len(threading.enumerate())>20):
 		time.sleep(2)
 		print 'full'
 f1.close()
 f2.close()
+f3.close()
