a7e8518d467c3a90b54ddcdb283cbc0a54f83e45
wanderingJoker <pkucs.lsl@gmail.com>
2013-11-18 14:29:14 +0800

    Makefile

--- baitour/test.cc
+++ baitour/test.cc
@@ -1,16 +1,18 @@
-#include<iostream>
-#include<cstdio>
-#include<string>
-#include "gsoap/soapBaitourServiceSoapProxy.h"
-#include "gsoap/BaitourServiceSoap.nsmap"
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include "./gsoap/soapBaitourServiceSoapProxy.h"
+#include "./gsoap/BaitourServiceSoap.nsmap"
+#include "./gsoap/soapBaitourServiceSoapService.h"
 
 using namespace std;
 int main(int argc, const char *argv[])
 {
-	BaitourServiceSoapProxy proxy;
-	_ns1__GetAllCommonPolicy ns1__GetAllCommonPolicy;
-	_ns1__GetAllCommonPolicyResponse ns1__GetAllCommonPolicyResponse;
-	proxy.GetAllCommonPolicy(ns1__GetAllCommonPolicy,ns1__GetAllCommonPolicyResponse);
-	print ns1__GetAllCommonPolicyResponse.
+	BaitourServiceSoapService p;
+// 	_ns1__GetAllCommonPolicy ns1__GetAllCommonPolicy;
+//	_ns1__GetAllCommonPolicyResponse ns1__GetAllCommonPolicyResponse;
+//	proxy.GetAllCommonPolicy(ns1__GetAllCommonPolicy,ns1__GetAllCommonPolicyResponse);
+//	print ns1__GetAllCommonPolicyResponse.
+	cout << "here"<< endl;
 	return 0;
 }
