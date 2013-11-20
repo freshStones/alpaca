e404150d7ad0ffea44436b6016cf0eae72b05cea
zghember <zghpku@gmail.com>
2013-11-20 15:25:14 +0800

    commit

--- baitour/test.cc
+++ baitour/test.cc
@@ -11,9 +11,11 @@ int main(int argc, const char *argv[])
 {
 	BaitourServiceSoapProxy proxy;
 	_ns1__GetAllCommonPolicy ns1__GetAllCommonPolicy;
-	string a = "0",b = "0";
+	string a = "0",b = "0",c = "lhjk",d = "lhjk";
 	ns1__GetAllCommonPolicy.tripType = &a;
 	ns1__GetAllCommonPolicy.ticketType = &b;
+	ns1__GetAllCommonPolicy.agentUserName = &c;
+	ns1__GetAllCommonPolicy.pwd = &d;
 	_ns1__GetAllCommonPolicyResponse ns1__GetAllCommonPolicyResponse;
 	proxy.GetAllCommonPolicy(&ns1__GetAllCommonPolicy,&ns1__GetAllCommonPolicyResponse);
 	cout << *(ns1__GetAllCommonPolicyResponse.GetAllCommonPolicyResult);
