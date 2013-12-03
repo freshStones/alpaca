c6777475b093b16c991f9028cd788f3e8aa6f85e
zghember <zghpku@gmail.com>
2013-12-03 12:08:44 +0800

    sql

---
+++ sql/user.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `LH_AirTicket` /*!40100 DEFAULT CHARACTER SET gb2312 COLLATE gb2312_bin */;
+USE `LH_AirTicket`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 162.105.81.242    Database: LH_AirTicket
+-- ------------------------------------------------------
+-- Server version	5.5.31-0ubuntu0.12.04.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `userList`
+--
+
+DROP TABLE IF EXISTS `userList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userList` (
+  `idUserList` int(11) NOT NULL AUTO_INCREMENT,
+  `userAccount` varchar(45) COLLATE gb2312_bin NOT NULL,
+  `userPassport` varchar(45) COLLATE gb2312_bin NOT NULL,
+  `grantedBy` varchar(45) COLLATE gb2312_bin NOT NULL,
+  `authorityType` varchar(45) COLLATE gb2312_bin NOT NULL,
+  `lastModified` datetime NOT NULL,
+  PRIMARY KEY (`idUserList`),
+  UNIQUE KEY `idUserList_UNIQUE` (`idUserList`),
+  UNIQUE KEY `userAccount_UNIQUE` (`userAccount`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=gb2312 COLLATE=gb2312_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userList`
+--
+
+LOCK TABLES `userList` WRITE;
+/*!40000 ALTER TABLE `userList` DISABLE KEYS */;
+INSERT INTO `userList` VALUES (1,'xiaosb','xiaosb','hehe','all','0000-00-00 00:00:00'),(2,'admin','lh2013','joker','all','0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `userList` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-03 12:07:30
