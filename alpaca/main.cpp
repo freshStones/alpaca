2dae790f9d870ec5a70d0bc8d9ea2b8cc03e3ab9
zghember <zghpku@gmail.com>
2014-08-09 01:02:55 +0800

    Structural adjustment

--- alpaca/main.cpp
+++ alpaca/main.cpp
@@ -1,13 +1,43 @@
+//****************************************************************************************************************
+//*     This Qt based program is producted by Team 2022, with Hainan Langhua Business Travel Service Co. Ltd.    *
+//*                                    Generated in: 11/28/2013                                                  *
+//*                                 Members: joker, zghember, god miao                                           *
+//*                          Partner: Hainan Langhua Business Travel Service Co. Ltd.                            *
+//*                                      "Winter in PKU is beautiful!"                                           *
+//****************************************************************************************************************
+
 #include "mainwindow.h"
-#include <QApplication>
-#include <qunar/qunarpolicyapi.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::addLibraryPath("./plugins");
+
+//避免中文乱码
+#ifdef Q_OS_WIN32
+    QTextCodec *codec = QTextCodec::codecForName("System");
+#else
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+#endif
+
+    QTextCodec::setCodecForLocale(codec);
+
+//避免多个程序实例
+#ifdef Q_OS_WIN
+    QSharedMemory shrMemo;
+    shrMemo.setKey("LHOneInstanceLock");
+    if(!shrMemo.create(1))
+    {
+        QMessageBox::warning(NULL,QObject::tr("Warning"),QObject::tr("已经启动本程序的另一个实例！"),QMessageBox::Ok);
+        return -1;
+    }
+#endif
+
+    //添加浪花logo
+    QIcon icon;
+    icon.addFile(QStringLiteral(":/images/images/icon.png"), QSize(), QIcon::Normal, QIcon::Off);
+    a.setWindowIcon(icon);
+
     MainWindow w;
-    w.show();
-    QunarPolicyAPI qunarapi;
-    qunarapi.domToFile(qunarapi.uploadPolicy());
     return a.exec();
 }
--- alpaca/mainwindow.h
+++ alpaca/mainwindow.h
@@ -2,7 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QApplication>
+#include <QtCore/qglobal.h>
+#include <QTextCodec>
+#include <QObject>
+#include <QSharedMemory>
+#include <QMessageBox>
+#include <QSqlTableModel>
+#include <QPair>
+#include <QVector>
 
+#include "btdatabase.h"
+#include "login.h"
+#include "adminwindow.h"
+#include "dump.h"
 namespace Ui {
 class MainWindow;
 }
@@ -10,13 +23,35 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void signalConnection();
+    void debug();
+    void init();
+    void loadSpaceInfo();
+    void setDiagMidParent(int height, int width);
+
+private slots:
+    void on_queryButton_clicked();
+    void on_dumpButton_clicked();
+    void on_action_accountProc_triggered();
+    void on_action_logOut_triggered();
+
+    void on_action_exit_triggered();
 
 private:
+    dump *dumpExcelOp;
+    QString username;
+    Login *loginWindow;
+    AdminWindow *adminWindow;
     Ui::MainWindow *ui;
+    QSqlTableModel *allPolicyModel;
+    QVector<QPair<QString,QString> > qv[11];  //account from 0.9,0.85 to 0.4
+
+public slots:
+    void slotLoggedin(QString, QString);
 };
 
 #endif // MAINWINDOW_H
